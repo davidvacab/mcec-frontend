a76efb7040baab4ad220225b3b61dc17006562fc
David Israel <davidvacab@gmail.com>
2023-07-01 09:44:09 -0400

    Installed Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
-import Alert from "./components/Alert";
 import axios from "axios";
-import Button from "./components/Button";
-import Like from "./components/Like";
 import { useEffect, useState } from "react";
-
-interface User {
-  id: number;
-  name: string;
-}
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  const [showAlert, setShowAlert] = useState(false);
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
-  }, []);
-
-  return (
-    <div>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <Button colorScheme="blue">Button</Button>;
 }
 
 export default App;
