59a6f839ae4f3b05ebaff42dc191684f76e879c1
David Israel <davidvacab@gmail.com>
2023-07-03 14:31:03 -0400

    Show spinner when loading Topics

--- src/Hymnbook/components/TopicList.tsx
+++ src/Hymnbook/components/TopicList.tsx
@@ -1,13 +1,19 @@
-import { List, ListItem } from "@chakra-ui/react";
+import { List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useTopics from "../hooks/useTopics";
 
 const TopicList = () => {
   const { data, error, isLoading } = useTopics();
 
+  {
+    error && <Text>{error}</Text>;
+  }
+  if (isLoading) return <Spinner />;
   return (
     <List>
       {data.map((topic) => (
-        <ListItem key={topic.id} paddingY={1} fontSize={'lg'}>{topic.title}</ListItem>
+        <ListItem key={topic.id} paddingY={1} fontSize={"lg"}>
+          {topic.title}
+        </ListItem>
       ))}
     </List>
   );
